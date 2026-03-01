b0821c378f8447c0380b05dbf091cfcc5a4f3991
gadisa kasaye <gadisaka04@gmail.com>
2026-03-01 10:31:00 +0300

    Update API_URL in both admin and frontend constants to point to the new production URL on Render.

--- admin/src/constant.js
+++ admin/src/constant.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000";
-// export const API_URL = "https://onlinebingo-ko5s.onrender.com";
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "https://kal-bingo.onrender.com";
 // export const API_URL = "https://api.sheqaygames.com";
