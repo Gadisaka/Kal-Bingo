36e7cf724f03e9a1c12ccf9b399c459113902fe2
gadisa kasaye <gadisaka04@gmail.com>
2026-03-30 10:39:50 +0300

    Update API_URL in constants for both admin and frontend to point to the new API endpoint at https://api.kal.goldbingo.net

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,5 @@
 // export const API_URL = "http://localhost:5000";
-export const API_URL = "https://kal-bingo.onrender.com";
+// export const API_URL = "https://kal-bingo.onrender.com";
+export const API_URL = "https://api.kal.goldbingo.net";
 
 export const TELEGRAM_BOT_USERNAME = import.meta.env.VITE_BOT_USERNAME;
